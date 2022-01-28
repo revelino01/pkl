e43cda94ef155d0b4d2286bd0434369c962aec18
revelino01 <revelino.adli@gmail.com>
2022-01-29 01:36:54 +0700

    master

--- RecognisiType2.py
+++ RecognisiType2.py
@@ -16,7 +16,7 @@ while True:
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x1, y2, x2 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-        cv2.putText(frame, name, (x1,y1 - 10), cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+        cv2.putText(frame, name, (x1,y1 - 10), cv2.FONT_HERSHEY_DUPLEX,1,(0,255,255),2)
         cv2.rectangle(frame,(x1,y1),(x2,y2),(255,255,0), 4)
         
     cv2. imshow("Frame",frame)
